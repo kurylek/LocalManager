2dd4e314e895a176c90118309ae926a2b75c41bb
Kamil Kuryłek <kurylek98@gmail.com>
2023-07-20 20:17:24 +0200

    Generate backend with Spring Initializr

---
+++ Backend/src/test/java/com/kurylek/locamanager/LocalManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kurylek.locamanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
