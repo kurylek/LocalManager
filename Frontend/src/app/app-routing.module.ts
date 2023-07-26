a002e269a0e380df4a7ee48c89c42921fbd88ae5
Kamil Kuryłek <kurylek98@gmail.com>
2023-07-26 18:55:11 +0200

    Add nav header and use router outlet

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PrinterListComponent } from './components/printer-list/printer-list.component';
+import { OrdersComponent } from './components/orders/orders.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: OrdersComponent },
+  { path: 'printers', component: PrinterListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -5,9 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PrinterListModule } from './components/printer-list/printer-list.module';
+import { HeaderComponent } from './components/header/header.component';
+import { OrdersComponent } from './components/orders/orders.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, OrdersComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
